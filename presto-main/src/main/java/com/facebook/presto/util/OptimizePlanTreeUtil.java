96194c53d9167ce062af9b9bc2865804e727b935
yuliang5 <yuliang5@iflytek.com>
2019-01-28 10:51:45 +0800

    fix bug for optimeze table plantree with alias

--- presto-main/src/main/java/com/facebook/presto/util/OptimizePlanTreeUtil.java
+++ presto-main/src/main/java/com/facebook/presto/util/OptimizePlanTreeUtil.java
@@ -48,95 +48,7 @@ public class OptimizePlanTreeUtil {
     private static void genPlanSourceSql(String sessionCatalog, String sessionSchema, Map<String, OptimizeTable> allSourceSqls, QuerySpecification planTree) {
         Relation from = planTree.getFrom().get();
         if (from instanceof Table) {
-            Select select = planTree.getSelect();
-            Table table = (Table) from;
-            String fullTableName = (table.getName().getParts().size() == 1 ? sessionCatalog + "." + sessionSchema + "." : "") + table.getName().toString();
-            String catalog = table.getName().getParts().size() == 1 ? sessionCatalog : table.getName().getParts().get(0);
-            if (select != null) {
-                Optional<Expression> where = planTree.getWhere();
-                Optional<GroupBy> groupBy = planTree.getGroupBy();
-                Optional<Expression> having = planTree.getHaving();
-                Optional<OrderBy> orderBy = planTree.getOrderBy();
-                Optional<String> limit = planTree.getLimit();
-
-                List<SelectItem> reConstructSelect = new ArrayList<>();
-                List<String> fields = new ArrayList<>();
-                for (int index = 0;index < select.getSelectItems().size();index++) {
-                    SelectItem field = select.getSelectItems().get(index);
-                    if (field instanceof SingleColumn) {
-                        SingleColumn singleColumn = (SingleColumn) field;
-                        if (singleColumn.getExpression() instanceof FunctionCall) {
-                            FunctionCall functionCall = (FunctionCall) singleColumn.getExpression();
-                            if (catalog.toLowerCase(Locale.getDefault()).contains("kylin") || catalog.toLowerCase(Locale.getDefault()).contains("druid")) {
-                                Expression column;
-                                if (functionCall.getArguments().size() > 0) {
-                                    column = functionCall.getArguments().get(0);
-                                } else {
-                                    throw new SemanticException(SemanticErrorCode.NOT_SUPPORTED, singleColumn, "kylin or druid '%s' cannot be supported", "count(*)");
-                                }
-                                reConstructSelect.add(new SingleColumn(column));
-                                fields.add(singleColumn.toString().replaceAll("\"", ""));
-                            } else {
-                                reConstructSelect.add(singleColumn);
-                                fields.add(singleColumn.toString().replaceAll("\"", ""));
-                            }
-                        } else {
-                            reConstructSelect.add(singleColumn);
-                            fields.add(singleColumn.toString());
-                        }
-                    } else if (field instanceof AllColumns) {
-                        reConstructSelect.add(field);
-                    }
-                }
-
-                StringBuilder sql = new StringBuilder();
-                sql.append("select").append(StringUtils.SPACE);
-                if (select.isDistinct()) {
-                    sql.append("distinct").append(StringUtils.SPACE);
-                }
-                if (catalog.toLowerCase(Locale.getDefault()).contains("kylin") || catalog.toLowerCase(Locale.getDefault()).contains("druid")) {
-                    planTree.setSelect(new Select(select.getLocation().get(), select.isDistinct(), reConstructSelect));
-                }
-                sql.append("{columns}").append(StringUtils.SPACE);
-                sql.append("from").append(StringUtils.SPACE).append("{tableName}").append(StringUtils.SPACE);
-                if (where.isPresent()) {
-                    sql.append("where").append(StringUtils.SPACE).append(where.get().toString().replaceAll("\"", "")).append(StringUtils.SPACE);
-                    if (catalog.toLowerCase(Locale.getDefault()).contains("kylin") || catalog.toLowerCase(Locale.getDefault()).contains("druid")) {
-                        planTree.setWhere(Optional.empty());
-                    }
-                }
-                if (groupBy.isPresent()) {
-                    List<String> groups = new ArrayList<>();
-                    List<GroupingElement> groupElements = groupBy.get().getGroupingElements();
-                    for (GroupingElement item : groupElements) {
-                        for (Node node : item.getChildren()) {
-                            groups.add(node.toString());
-                        }
-                    }
-                    sql.append("group by").append(StringUtils.SPACE).append(StringUtils.join(groups, ",")).append(StringUtils.SPACE);
-                    if (catalog.toLowerCase(Locale.getDefault()).contains("kylin") || catalog.toLowerCase(Locale.getDefault()).contains("druid")) {
-                        planTree.setGroupBy(Optional.empty());
-                    }
-                    if (having.isPresent()) {
-                        sql.append("having").append(StringUtils.SPACE).append(having.get().toString().replaceAll("\"", "")).append(StringUtils.SPACE);
-                        if (catalog.toLowerCase(Locale.getDefault()).contains("kylin") || catalog.toLowerCase(Locale.getDefault()).contains("druid")) {
-                            planTree.setHaving(Optional.empty());
-                        }
-                    }
-                }
-                if (orderBy.isPresent()) {
-                    List<String> sorts = new ArrayList<>();
-                    List<SortItem> sortItems = orderBy.get().getSortItems();
-                    for (SortItem item : sortItems) {
-                        sorts.add(item.getSortKey().toString() + StringUtils.SPACE + (item.getOrdering() == SortItem.Ordering.DESCENDING ? "desc" : ""));
-                    }
-                    sql.append("order by").append(StringUtils.SPACE).append(StringUtils.join(sorts, ",")).append(StringUtils.SPACE);
-                }
-                if (limit.isPresent()) {
-                    sql.append("limit").append(StringUtils.SPACE).append(limit.get());
-                }
-                allSourceSqls.put(fullTableName, new OptimizeTable(sql.toString(), fields));
-            }
+            optimizeTable(sessionCatalog, sessionSchema, allSourceSqls, planTree);
         } else if (from instanceof TableSubquery) {
             TableSubquery tableSubquery = (TableSubquery) from;
             genPlanSourceSql(sessionCatalog, sessionSchema, allSourceSqls, (QuerySpecification) tableSubquery.getQuery().getQueryBody());
@@ -144,9 +56,103 @@ public class OptimizePlanTreeUtil {
             AliasedRelation aliasedRelation = (AliasedRelation) from;
             if (aliasedRelation.getRelation() instanceof TableSubquery) {
                 genPlanSourceSql(sessionCatalog, sessionSchema, allSourceSqls, (QuerySpecification) ((TableSubquery) aliasedRelation.getRelation()).getQuery().getQueryBody());
+            } else if (aliasedRelation.getRelation() instanceof Table) {
+                optimizeTable(sessionCatalog, sessionSchema, allSourceSqls, planTree);
             }
         } else if (from instanceof Join) {
             deepTraversalJoin(sessionCatalog, sessionSchema, allSourceSqls, (Join) from);
+        }
+    }
+
+    private static void optimizeTable(String sessionCatalog, String sessionSchema, Map<String, OptimizeTable> allSourceSqls, QuerySpecification planTree) {
+        Select select = planTree.getSelect();
+        Table table = planTree.getFrom().get() instanceof Table ? (Table) planTree.getFrom().get() : (Table) ((AliasedRelation) planTree.getFrom().get()).getRelation();
+        String fullTableName = (table.getName().getParts().size() == 1 ? sessionCatalog + "." + sessionSchema + "." : "") + table.getName().toString();
+        String catalog = table.getName().getParts().size() == 1 ? sessionCatalog : table.getName().getParts().get(0);
+        if (select != null) {
+            Optional<Expression> where = planTree.getWhere();
+            Optional<GroupBy> groupBy = planTree.getGroupBy();
+            Optional<Expression> having = planTree.getHaving();
+            Optional<OrderBy> orderBy = planTree.getOrderBy();
+            Optional<String> limit = planTree.getLimit();
+
+            List<SelectItem> reConstructSelect = new ArrayList<>();
+            List<String> fields = new ArrayList<>();
+            for (int index = 0;index < select.getSelectItems().size();index++) {
+                SelectItem field = select.getSelectItems().get(index);
+                if (field instanceof SingleColumn) {
+                    SingleColumn singleColumn = (SingleColumn) field;
+                    if (singleColumn.getExpression() instanceof FunctionCall) {
+                        FunctionCall functionCall = (FunctionCall) singleColumn.getExpression();
+                        if (catalog.toLowerCase(Locale.getDefault()).contains("kylin") || catalog.toLowerCase(Locale.getDefault()).contains("druid")) {
+                            Expression column;
+                            if (functionCall.getArguments().size() > 0) {
+                                column = functionCall.getArguments().get(0);
+                            } else {
+                                throw new SemanticException(SemanticErrorCode.NOT_SUPPORTED, singleColumn, "kylin or druid '%s' cannot be supported", "count(*)");
+                            }
+                            reConstructSelect.add(new SingleColumn(column));
+                            fields.add(singleColumn.toString().replaceAll("\"", ""));
+                        } else {
+                            reConstructSelect.add(singleColumn);
+                            fields.add(singleColumn.toString().replaceAll("\"", ""));
+                        }
+                    } else {
+                        reConstructSelect.add(singleColumn);
+                        fields.add(singleColumn.toString());
+                    }
+                } else if (field instanceof AllColumns) {
+                    reConstructSelect.add(field);
+                }
+            }
+
+            StringBuilder sql = new StringBuilder();
+            sql.append("select").append(StringUtils.SPACE);
+            if (select.isDistinct()) {
+                sql.append("distinct").append(StringUtils.SPACE);
+            }
+            if (catalog.toLowerCase(Locale.getDefault()).contains("kylin") || catalog.toLowerCase(Locale.getDefault()).contains("druid")) {
+                planTree.setSelect(new Select(select.getLocation().get(), select.isDistinct(), reConstructSelect));
+            }
+            sql.append("{columns}").append(StringUtils.SPACE);
+            sql.append("from").append(StringUtils.SPACE).append("{tableName}").append(StringUtils.SPACE);
+            if (where.isPresent()) {
+                sql.append("where").append(StringUtils.SPACE).append(where.get().toString().replaceAll("\"", "")).append(StringUtils.SPACE);
+                if (catalog.toLowerCase(Locale.getDefault()).contains("kylin") || catalog.toLowerCase(Locale.getDefault()).contains("druid")) {
+                    planTree.setWhere(Optional.empty());
+                }
+            }
+            if (groupBy.isPresent()) {
+                List<String> groups = new ArrayList<>();
+                List<GroupingElement> groupElements = groupBy.get().getGroupingElements();
+                for (GroupingElement item : groupElements) {
+                    for (Node node : item.getChildren()) {
+                        groups.add(node.toString());
+                    }
+                }
+                sql.append("group by").append(StringUtils.SPACE).append(StringUtils.join(groups, ",")).append(StringUtils.SPACE);
+                if (catalog.toLowerCase(Locale.getDefault()).contains("kylin") || catalog.toLowerCase(Locale.getDefault()).contains("druid")) {
+                    planTree.setGroupBy(Optional.empty());
+                }
+                if (having.isPresent()) {
+                    sql.append("having").append(StringUtils.SPACE).append(having.get().toString().replaceAll("\"", "")).append(StringUtils.SPACE);
+                    if (catalog.toLowerCase(Locale.getDefault()).contains("kylin") || catalog.toLowerCase(Locale.getDefault()).contains("druid")) {
+                        planTree.setHaving(Optional.empty());
+                    }
+                }
+            }
+            if (orderBy.isPresent()) {
+                List<String> sorts = new ArrayList<>();
+                List<SortItem> sortItems = orderBy.get().getSortItems();
+                for (SortItem item : sortItems) {
+                    sorts.add(item.getSortKey().toString() + StringUtils.SPACE + (item.getOrdering() == SortItem.Ordering.DESCENDING ? "desc" : ""));
+                }
+                sql.append("order by").append(StringUtils.SPACE).append(StringUtils.join(sorts, ",")).append(StringUtils.SPACE);
+            }
+            if (limit.isPresent()) {
+                sql.append("limit").append(StringUtils.SPACE).append(limit.get());
+            }
+            allSourceSqls.put(fullTableName, new OptimizeTable(sql.toString(), fields));
         }
     }
 
